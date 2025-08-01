fefe673792f793cec7f5bbc25d04c1393d93f743
swetraj <and90swet@gmail.com>
2025-08-01 08:01:51 +0545

    [ main ] Updated DDD Architecture in process

--- api/v1/auth/dto.go
+++ api/v1/auth/dto.go
@@ -13,3 +13,9 @@ type LoginRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
+
+type LoginResponse struct {
+	ID       uint   `json:"id"`
+	Email    string `json:"email"`
+	IsActive bool   `json:"is_active"`
+}
